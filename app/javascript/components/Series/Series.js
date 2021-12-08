c89a12fbab5bcef9cd760ebc1a319aff6ac94cad
Gergo Karcagi <karcagigergo@gmail.com>
2021-12-08 14:39:47 -0300

    testing if axios is working - done

--- app/javascript/components/Series/Series.js
+++ app/javascript/components/Series/Series.js
@@ -1,20 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
 
 const Series = () => {
-  const [series, setSeries] = useState([])
+  const [series, setSeries] = useState([]);
 
   useEffect(() => {
     //Get all of our airlines from api
     //Update airlines in our state
-    axios.get('/api/v1/series')
-      .then(resp => console.log(resp))
-      .catch(resp => console.log(resp))
-  }, [series.lenght])
+    axios
+      .get("/api/v1/series")
+      .then((resp) => {
+        setSeries(resp.data.data);
+      })
+      .catch((resp) => console.log(resp));
+  }, [series.lenght]);
+
+  const list = series.map((item) => {
+    return <li key={item.attributes.name}>{item.attributes.name}</li>;
+  });
 
   return (
-    <div>This is the Series#index view for our app!</div>
-  )
-}
+    <Fragment>
+      <div>This is the Series#index view for our app!</div>
+      <ul>{list}</ul>
+    </Fragment>
+  );
+};
 
-export default Series
+export default Series;
